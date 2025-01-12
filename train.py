c47a265c9503f630e636a799fdc8f8c69ff05602
rahul raj <rahul.rajkumar@rutgers.edu>
2025-01-12 18:54:37 -0500

    need data

--- train.py
+++ train.py
@@ -1,8 +1,11 @@
 import numpy as np
 
 def KMeans(x, y, clusters):
-    sample_x = x
-    sample_y = y
+
     sample_list = []
     for i in range(len(clusters)):
-        
+       sample_list.append([x[i], y[i]])
+
+    return sample_list
+
+testing = KMeans()
